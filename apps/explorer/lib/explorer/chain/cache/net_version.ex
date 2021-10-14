12ca26ff3c0f33d6a8a094eb9f753feb7a8a2332
Leandre <leandre.china@gmail.com>
2021-10-14 20:36:54 +0800

    ttt

--- apps/explorer/lib/explorer/chain/cache/net_version.ex
+++ apps/explorer/lib/explorer/chain/cache/net_version.ex
@@ -12,8 +12,10 @@ defmodule Explorer.Chain.Cache.NetVersion do
   defp handle_fallback(:version) do
     case EthereumJSONRPC.fetch_net_version(Application.get_env(:explorer, :json_rpc_named_arguments)) do
       {:ok, value} ->
+        Logger.info(["netversion111111:#{value}}"])
         if value == 1 do
           value = 321
+          Logger.info(["netversion222:#{value}}"])
         end
         {:update, value}
 
