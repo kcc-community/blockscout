6283602c224fe300d02a6ed7e78e385de14a685c
Leandre <leandre.china@gmail.com>
2021-10-14 20:53:27 +0800

    321

--- apps/explorer/lib/explorer/chain/cache/net_version.ex
+++ apps/explorer/lib/explorer/chain/cache/net_version.ex
@@ -6,24 +6,24 @@ defmodule Explorer.Chain.Cache.NetVersion do
   require Logger
 
   use Explorer.Chain.MapCache,
-    name: :net_version,
-    key: :version
+      name: :net_version,
+      key: :version
 
   defp handle_fallback(:version) do
     case EthereumJSONRPC.fetch_net_version(Application.get_env(:explorer, :json_rpc_named_arguments)) do
       {:ok, value} ->
-        Logger.info(["netversion111111:#{value}}"])
         if value == 1 do
-          value = 321
-          Logger.info(["netversion222:#{value}}"])
+          {:update, 321}
+        else
+          {:update, value}
         end
-        {:update, value}
-        Logger.info(["netversion333:#{value}}"])
 
       {:error, reason} ->
-        Logger.debug([
-          "Coudn't fetch net_version, reason: #{inspect(reason)}"
-        ])
+        Logger.debug(
+          [
+            "Coudn't fetch net_version, reason: #{inspect(reason)}"
+          ]
+        )
 
         {:return, nil}
     end
